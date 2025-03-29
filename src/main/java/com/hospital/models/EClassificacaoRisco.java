fe9fdd133c22b6cebbb7b00e554508bd42061354
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2025-03-29 11:32:47 -0300

    feat: add EClassificacaoRisco

---
+++ src/main/java/com/hospital/models/EClassificacaoRisco.java
@@ -0,0 +1,33 @@
+package src.main.java.com.hospital.models;
+
+public enum EClassificacaoRisco {
+    VERMELHO(1, "Necessitam de atendimento imediato. CASOS DE EMERGÊNCIA"),
+    LARANJA(2, "Necessitam de atendimento praticamente imediato. CASOS MUITO URGENTES"),
+    AMARELO(3, "Necessitam de atendimento rápido, mas podem aguardar. CASOS DE URGÊNCIA"),
+    VERDE(4, "Podem aguardar atendimento ou serem encaminhados. CASOS POUCO URGENTES"),
+    AZUL(5, "Podem aguardar ou serem encaminhados. CASOS NÃO URGENTES");
+
+    private final int nivel;
+    private final String descricao;
+
+    EClassificacaoRisco(int nivel, String descricao) {
+        this.nivel = nivel;
+        this.descricao = descricao;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "EClassificacaoRisco{" +
+                "nivel=" + nivel +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
